d3395d99038dfbf34e237ce7894e967c77bcff91
MaikHenriqueSP <maik.hsp.contato@gmail.com>
2021-05-06 11:00:18 -0300

    refactor(main): implementation of a method that returns the anchor tag by the header title

--- main.py
+++ main.py
@@ -12,9 +12,18 @@ def get_request_content(request_url):
 def get_request_url(end_point_url):
     return f'''{BASE_URL}{end_point_url}'''
 
+def get_anchor_by_header_title(header_title, content):
+    soup = BeautifulSoup(content, "html.parser")
+    content_container = soup.find("div", class_= "item_page")
+    header = soup.find("h2", string=re.compile(header_title))
+    sibling_container = header.find_next_sibling("div", class_="alert alert-icolink")
+    return sibling_container.a
+
 def get_download_url(base_url = BASE_URL):    
     request_url = get_request_url(TISS_ENDPOINT_URL)
     content = get_request_content(request_url)
+
+    get_anchor_by_header_title("Padrão TISS – Versão", content)
 
 def main():
     get_download_url()
