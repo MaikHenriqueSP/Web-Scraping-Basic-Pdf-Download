b75dd9573509d4c546e35aa4b64e43ef670c5ebc
MaikHenriqueSP <maik.hsp.contato@gmail.com>
2021-05-06 10:53:49 -0300

    refactor(request url): extraction of the url builder method

--- main.py
+++ main.py
@@ -3,16 +3,18 @@ import requests
 import pathlib
 
 BASE_URL = "http://www.ans.gov.br"
-
+TISS_ENDPOINT_URL = "/prestadores/tiss-troca-de-informacao-de-saude-suplementar"
 
 def get_request_content(request_url):
     response = requests.get(request_url)
     return response.content
 
-def get_download_url(base_url = BASE_URL):
-    tiss_url = "/prestadores/tiss-troca-de-informacao-de-saude-suplementar"
-    request_url = f'''{BASE_URL}{tiss_url}'''
-    get_request_content(request_url)
+def get_request_url(end_point_url):
+    return f'''{BASE_URL}{end_point_url}'''
+
+def get_download_url(base_url = BASE_URL):    
+    request_url = get_request_url(TISS_ENDPOINT_URL)
+    content = get_request_content(request_url)
 
 def main():
     get_download_url()
