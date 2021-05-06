cdd631a1dedb21cd976f3cdd02b1ace29176d74c
MaikHenriqueSP <maik.hsp.contato@gmail.com>
2021-05-06 10:47:57 -0300

    feat(download url): creation of the request url argument along with the request conent function

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
+from bs4 import BeautifulSoup
+import requests
+import pathlib
+
+BASE_URL = "http://www.ans.gov.br"
+
+
+
+def get_download_url(base_url = BASE_URL):
+    tiss_url = "/prestadores/tiss-troca-de-informacao-de-saude-suplementar"
+    request_url = f'''{BASE_URL}{tiss_url}'''
+    get_request_content(request_url)
+
 def main():
+    get_download_url()
     
 
 if __name__ == "__main__":
