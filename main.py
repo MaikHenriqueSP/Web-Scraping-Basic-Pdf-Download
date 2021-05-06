86b49b7899497956808a8d9ee6761f7a063938e8
MaikHenriqueSP <maik.hsp.contato@gmail.com>
2021-05-06 10:49:15 -0300

    feat(content requestor): implementation of the content requestor function

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ import pathlib
 BASE_URL = "http://www.ans.gov.br"
 
 
+def get_request_content(request_url):
+    response = requests.get(request_url)
+    return response.content
 
 def get_download_url(base_url = BASE_URL):
     tiss_url = "/prestadores/tiss-troca-de-informacao-de-saude-suplementar"
