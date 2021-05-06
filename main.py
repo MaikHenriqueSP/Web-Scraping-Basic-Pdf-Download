636439de92021a72402afcbafd2acd1ed57ba7db
MaikHenriqueSP <maik.hsp.contato@gmail.com>
2021-05-06 12:19:07 -0300

    docs(base): base project documentation

--- main.py
+++ main.py
@@ -1,10 +1,33 @@
+# -*- coding: utf-8 -*-
 from util import *
 import re
 
+'''
+@author: Maik Henrique
+This mini-project aims to demonstrate the basics of web-scrapping, it simply works by getting into the target page
+and then looking for the most recent version page of the pdf we aim to download, it then go to the download page and
+download it to a local folder.
+'''
+
+'''
+Attributes:
+    BASE_URL: Defines the base target url, whiche is then combined with different endpoints as the execution progress
+    TISS_ENDPOINT_URL: Defines the initial endpoint which will be scrapped in order to extract the download page endpoint
+    TARGET_TABLE_ROW_TITLE: Defines the target row table title which we aim to extract the content
+'''
 BASE_URL = "http://www.ans.gov.br"
 TISS_ENDPOINT_URL = "/prestadores/tiss-troca-de-informacao-de-saude-suplementar"
 TARGET_TABLE_ROW_TITLE = "Componente Organizacional"
 
+
+'''
+    Args:
+        header_title (string): A string which must be the target title of the container
+        content (bytes): A payload in bytes representing the response from the request
+    Returns: 
+        The anchor tag that is related to the header_title
+
+'''
 def get_anchor_by_header_title(header_title, content):
     soup = get_soup_instance(content)
     content_container = soup.find("div", class_= "item_page")
@@ -13,13 +36,27 @@ def get_anchor_by_header_title(header_title, content):
 
     return sibling_container.a
 
+'''
+    Args:
+        base_url (string): Url the will be requests withing the target endpoint
+    Returns:
+        download_url (string): The endpoint of the download page
+
+'''
 def get_download_url(base_url = BASE_URL):    
-    request_url = get_request_url(BASE_URL,TISS_ENDPOINT_URL)
+    request_url = get_request_url(BASE_URL, TISS_ENDPOINT_URL)
     content = get_request_content(request_url)
     anchor_tag = get_anchor_by_header_title("Padrão TISS – Versão", content)
 
     return anchor_tag["href"]
 
+'''
+    Args:
+        soup: BeautifulSoup instance entangled with the related content
+        target_title (string):  The title which will be checked against the row title
+    Returns:
+        pdf_url (string): The endpoint where the pdf can be downloaded
+'''
 def get_pdf_url(soup, target_title=TARGET_TABLE_ROW_TITLE ):
     table_container_div = soup.find("div", class_="table-responsive")
     table_body = table_container_div.table.tbody
@@ -28,6 +65,13 @@ def get_pdf_url(soup, target_title=TARGET_TABLE_ROW_TITLE ):
     anchor_tag = table_row.find("a")
 
     return anchor_tag["href"]
+
+'''
+    Write the pdf_content into a pdf file at the specified path 
+
+    Args:
+        pdf_content (bytes): A raw of bytes that are a response from the pdf request
+'''
 
 def save_pdf(pdf_content):
     folder_path = "./component_organizacional"
@@ -38,6 +82,10 @@ def save_pdf(pdf_content):
     with open(file_path, "wb") as f:        
         f.write(pdf_content)
 
+'''
+    Args:
+        download_endpoint(string): The endpoint where the pdf file is stored
+'''
 def download_pdf(download_enpoint):
     request_url = get_request_url(BASE_URL, download_enpoint)
     content = get_request_content(request_url)
--- util.py
+++ util.py
@@ -2,17 +2,47 @@ from bs4 import BeautifulSoup
 import requests
 import pathlib
 
+'''
+    Utilities file which holds the commonly used functions used throughout the application
+'''
+
+
+'''
+    Args:
+        request_url (string): Target url in which the request will be done
+    Returns:
+        The content payload in bytes
+'''
 def get_request_content(request_url):
     response = requests.get(request_url)
 
     return response.content
 
+'''
+    Args:
+        base_url (string): The url which is the first part of the concatenated url
+        end_point_url (string): Last part of the url 
+    Returns
+        A concatenated string of the url
+'''
 def get_request_url(base_url, end_point_url):
     return f'''{base_url}{end_point_url}'''
 
+
+'''
+    Args:
+        content (bytes): Content payload
+    Returns:
+        BeautifulSoup instance parsed with html.parser
+'''
 def get_soup_instance(content):
     soup = BeautifulSoup(content, "html.parser")
     return soup
 
+
+'''
+    Args:
+        path (string): The path where the folder should be created
+'''
 def create_folder_if_not_exists(path):
     pathlib.Path(path).mkdir(parents=True, exist_ok=True)
